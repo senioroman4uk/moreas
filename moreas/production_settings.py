83eb07a1b392407409625e5aee3a288f9afa8057
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2015-08-25 16:20:27 +0300

    fixing libraries

--- moreas/production_settings.py
+++ moreas/production_settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact'
+    'contact',
+    'products'
 )
 
 MIDDLEWARE_CLASSES = (
