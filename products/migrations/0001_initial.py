9f748219f6be00b5df9c269f7dc13779313e9950
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2015-08-25 16:10:45 +0300

    fixing libraries

---
+++ products/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('title', models.TextField(max_length=100)),
+                ('description', models.TextField(blank=True)),
+                ('image', models.ImageField(upload_to='')),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
